d5137289018b559c147abefa2e0a0f9b2e472084
ridhima2030 <ridhima.chaturvedi.cs27@iilm.edu>
2026-09-02 10:51:07 +0530

    Create Q8. Lowest Common Ancestor of a Binary Tree.py

---
+++ Q8. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        return left if left else right
