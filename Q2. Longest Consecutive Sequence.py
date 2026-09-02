225f689c3daf99d3a56690c42ffee8c5ef914d1c
ridhima2030 <ridhima.chaturvedi.cs27@iilm.edu>
2026-09-02 10:34:13 +0530

    Implement longest consecutive sequence function

---
+++ Q2. Longest Consecutive Sequence.py
@@ -0,0 +1,10 @@
+def longestConsecutive(nums):
+    num_set = set(nums)
+    longest = 0
+    for n in num_set:
+        if n - 1 not in num_set:  # start of a sequence
+            length = 1
+            while n + length in num_set:
+                length += 1
+            longest = max(longest, length)
+    return longest
